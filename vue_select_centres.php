d6e6136c1e842499298bc56577d6c16067354fe8
Adrien Rusaouen <157524725+Adruvix@users.noreply.github.com>
2025-03-13 01:38:24 +0100

    Update vue_select_centres.php

--- vue_select_centres.php
+++ vue_select_centres.php
@@ -1,30 +1,50 @@
 <h3> Liste des centres de vaccinations </h3>
 
-<table border="1"> 
-	<tr>
-		<td> ID Centre</td>
-		<td> Nom du Centre</td>
-		<td> Adresse </td>
-		<td> Type du Centre</td>
-		<td> Téléphone </td>
-		<td> Opérations </td>
-		 
-	</tr>
+<?php
+if (isset($_POST['modifier'])) {
+    $idcentre = $_POST['idcentre'];
+    $tab = [
+        'nom' => $_POST['nom'],
+        'adresse' => $_POST['adresse'],
+        'typecentre' => $_POST['typecentre'],
+        'telephone' => $_POST['telephone']
+    ];
 
-	<?php
-	foreach ($lesCentres as $unCentre) {
-		echo " <tr> "; 
-		echo " <td> ".$unCentre['idcentre']." </td> ";
-		echo " <td> ".$unCentre['nom']." </td> ";
-		echo " <td> ".$unCentre['adresse']." </td> ";
-		echo " <td> ".$unCentre['typecentre']." </td> ";
-		echo " <td> ".$unCentre['telephone']." </td> ";
-		echo " <td>  "; 
-		echo "<a href='index.php?page=2&action=sup&idcentre=".$unCentre['idcentre']."'> <img src='images/supprimer.png' height='30' width='30'> </a>";
-		echo "<a href='index.php?page=2&action=edit&idcentre=".$unCentre['idcentre']."'> <img src='images/editer.png' height='30' width='30'> </a>";  
+    editCentre($idcentre, $tab);
+    
+    // Redirection après modification
+    header("Location: index.php?page=2");
+    exit();
+}
+?>
 
-		echo " </td> ";
-		echo " </tr> ";
-	}
-	?>
+<table border="1">
+    <tr>
+        <th>ID Centre</th>
+        <th>Nom du Centre</th>
+        <th>Adresse</th>
+        <th>Type du Centre</th>
+        <th>Téléphone</th>
+        <th>Opérations</th>
+    </tr>
+
+    <?php
+    foreach ($lesCentres as $unCentre) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($unCentre['idcentre']) . "</td>";
+        echo "<td>" . htmlspecialchars($unCentre['nom']) . "</td>";
+        echo "<td>" . htmlspecialchars($unCentre['adresse']) . "</td>";
+        echo "<td>" . htmlspecialchars($unCentre['typecentre']) . "</td>";
+        echo "<td>" . htmlspecialchars($unCentre['telephone']) . "</td>";
+        echo "<td>";
+        echo "<a href='index.php?page=2&action=sup&idcentre=" . urlencode($unCentre['idcentre']) . "'>
+                <img src='images/supprimer.png' height='30' width='30' alt='Supprimer'>
+              </a> ";
+        echo "<a href='index.php?page=2&action=edit&idcentre=" . urlencode($unCentre['idcentre']) . "'>
+                <img src='images/editer.png' height='30' width='30' alt='Éditer'>
+              </a>";
+        echo "</td>";
+        echo "</tr>";
+    }
+    ?>
 </table>
