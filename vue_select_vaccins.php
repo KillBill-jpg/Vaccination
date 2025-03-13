615ae06080632db770371c03122cc706d3310c5f
Adrien Rusaouen <157524725+Adruvix@users.noreply.github.com>
2025-03-13 01:42:19 +0100

    Update vue_select_vaccins.php

--- vue_select_vaccins.php
+++ vue_select_vaccins.php
@@ -1,5 +1,24 @@
 <h3> Liste des vaccins </h3>
 
+<?php
+if (isset($_POST['modifier'])) {
+    $idvaccin = $_POST['idvaccin'];
+    $tab = [
+        'designation' => $_POST['designation'],
+        'dosage' => $_POST['dosage'],
+        'rappel' => $_POST['rappel'],
+        'c_indication' => $_POST['c_indication']
+		'laboratoire' => $_POST['laboratoire']
+    ];
+
+    editVaccin($idvaccin, $tab);
+    
+    // Redirection après modification
+    header("Location: index.php?page=2");
+    exit();
+}
+?>
+
 <table border="1"> 
 	<tr>
 		<td> ID Vaccin</td>
@@ -21,8 +40,8 @@
 		echo " <td> ".$unVaccin['c_indication']." </td> ";
 		echo " <td> ".$unVaccin['laboratoire']." </td> ";
 		echo " <td>  "; 
-		echo "<a href='index.php?page=2&action=sup&idvaccin=".$unVaccin['idvaccin']."'> <img src='images/supprimer.png' height='30' width='30'> </a>"; 
-
+		echo "<a href='index.php?page=3&action=sup&idvaccin=".$unVaccin['idvaccin']."'> <img src='images/supprimer.png' height='30' width='30'> </a>"; 
+		echo "<a href='index.php?page=3&action=edit&idpersonne=" . urlencode($unVaccin['idvaccin']) . "'> <img src='images/editer.png' height='30' width='30'> </a>";
 		echo " </td> ";
 		echo " </tr> ";
 	}
